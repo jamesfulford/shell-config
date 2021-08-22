ff8b615f090b6cf887e4f85fdbd8b66d49a4e07c
James Fulford <james.fulford@outlook.com>
2021-08-22 17:19:00 -0400

    feat(*): Adding aliases from laptop

--- common-aliases.sh
+++ common-aliases.sh
@@ -3,7 +3,35 @@
 alias ls="ls -FG"
 alias ll="ls -l"
 alias c=clear
+alias x=exit
+alias p="ping 8.8.8.8"
 
-up(){ DEEP=$1; for i in $(seq 1 ${DEEP:-"1"}); do cd ../; done; pwd;}
-bs(){ curl -s 'http://cbsg.sourceforge.net/cgi-bin/live' | grep -Eo '^<li>.*</li>' | sed "s@\</\\?li\>@@g" | gshuf -n 1 | cowsay;}
-function go(){ cd $1; ls; }
+alias aliases="subl ~/.bash_aliases"
+alias source-aliases="source ~/shell-config/common-aliases.sh"
+alias cbranch="git rev-parse --abbrev-ref HEAD | pbcopy"
+
+alias watch="watch " # so you can use aliases
+
+# https://www.sublimemerge.com/docs/command_line
+# but smerge is not installed by default
+alias sb="smerge ."
+
+# Git
+alias cont="git rebase --continue"
+alias abort="git rebase --abort"
+alias amend="git commit --amend --no-edit"
+
+function pull () {
+    git pull --rebase --autostash origin $(git rev-parse --abbrev-ref HEAD)
+}
+
+function patch () {
+    local branch=${1:-master}
+    git format-patch "$branch"
+}
+
+function clean-branches() {
+    git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d
+    git gc
+}
+ 
